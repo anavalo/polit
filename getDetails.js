62d65ff04ac9e0798d416c259a1878db55296802
Tasos Tsournos <tsournos.anastasios@gmail.com>
2022-01-16 10:07:59 +0200

    fix: try-catch

--- getDetails.js
+++ getDetails.js
@@ -10,43 +10,55 @@ const fn = async (data) => {
   await appendFile("final.csv", data);
 };
 
-async function main() {
-  
-  const fileStream = fs.createReadStream(FILE_TO_PARSE);
+function randomInteger(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
 
+async function main() {
+  const browser = await puppeteer.launch({
+    headless: true,
+  });
+  const page = await browser.newPage();
+  page.setDefaultNavigationTimeout(0);
+
+  const fileStream = fs.createReadStream(FILE_TO_PARSE);
   const rl = readline.createInterface({
     input: fileStream,
     crlfDelay: Infinity,
   });
-  
+
   for await (const line of rl) {
+    try {
+      await page
+        .goto(line)
+        .catch((e) => console.log(e, `PAGE PROBLEM at ${line}`));
 
-    const browser = await puppeteer.launch();
-    const page = await browser.newPage();
-    await page.goto(line).catch((e) => console.log(e, `PAGE PROBLEM at ${line}`));
-    const content = await page.content();
+      const content = await page.content();
+      const $ = cheerio.load(content);
 
-    const $ = cheerio.load(content);
+      const bookUrl = line;
+      const title = $(".details-right-column > h1").text();
+      const author = $(".details-right-column > b > a").text();
+      let recommendationsNum;
+      const recommendations = $(".product-reviews-inner").first();
 
-    const bookUrl = line;
-    const title = $(".details-right-column > h1").text();
-    const author = $(".details-right-column > b > a").text();
-    let recommendationsNum;
-    const recommendations = $(".product-reviews-inner").first();
+      if ($("h4", recommendations).text().startsWith("To βιβλίο")) {
+        recommendationsNum =
+          $(".product-reviews-inner").first().children().length - 1;
+      } else {
+        recommendationsNum = 0;
+      }
 
-    if ($("h4", recommendations).text().startsWith("To βιβλίο")) {
-      recommendationsNum =
-        $(".product-reviews-inner").first().children().length - 1;
-    } else {
-      recommendationsNum = 0;
+      const data = `${title}\t${author}\t${recommendationsNum}\t${bookUrl}\n`;
+      console.log(data);
+      fn(data);
+      await page.waitForTimeout(randomInteger(600, 2000));
+    } catch (e) {
+      console.log("ERROR EXCEPTION");
     }
-
-    const data = `${title}\t${author}\t${recommendationsNum}\t${bookUrl}\n`;
-    fn(data);
-
-    await browser.close();
-
   }
+
+  await browser.close();
 }
 
 main();
